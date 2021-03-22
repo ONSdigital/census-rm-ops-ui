f77c85fb7dab2c0fb6270de088b5e6dea7955edb
Ryan Grundy <ryan.grundy@ons.gov.uk>
2021-03-22 13:21:13 +0000

    Added back sorting and removed js

--- census_rm_ops_ui/views/case_details.py
+++ census_rm_ops_ui/views/case_details.py
@@ -14,10 +14,11 @@ def case_details_results():
 
     case_details = case_controller.get_all_case_details(case_id, current_app.config['CASE_API_URL'])
     case_events = case_details.pop('events')
-    for events in case_events:
+    sorted_case_events = sorted(case_events, key=lambda case_event: case_event['eventDate'])
+    for events in sorted_case_events:
         events['eventPayload'] = json.loads(events['eventPayload'])
 
-    return render_template('view_case_details.html', case_details=case_details, case_events=case_events)
+    return render_template('view_case_details.html', case_details=case_details, case_events=sorted_case_events)
 
 
 @case_details_bp.route('/link-qid/')
